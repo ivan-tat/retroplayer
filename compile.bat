201204e6781b7e6221d7c1e5bdfccc800756ed40
Ivan Tatarinov <ivan-tat@ya.ru>
2016-06-29 23:23:44 +0300

    expand asterisk with filenames

--- compile.bat
+++ compile.bat
@@ -1,15 +1,25 @@
+@echo off
+cls
+set PC=tpc -q
+set AS=tasm -t
 cd blaster
-bpc blaster.pas
+%PC% blaster.pas
 cd ..\main
-tasm *
-bpc emstool.pas
-bpc -U..\blaster s3mplay.pas
+%AS% dosproc.asm
+%AS% ems4fct.asm
+%AS% filldma.asm
+%AS% mixing.asm
+%AS% processo.asm
+%AS% readnote.asm
+%AS% volume.asm
+%PC% emstool.pas
+%PC% -U..\blaster s3mplay.pas
 cd ..
-bpc -Ublaster;main smalls3m
+%PC% -Ublaster;main smalls3m
 cd osci
-tasm *
-bpc -U..\blaster;..\main s3m_osci
+%AS% lines.asm
+%PC% -U..\blaster;..\main s3m_osci
 cd ..\player
-tasm *
-bpc -U..\blaster;..\main plays3m
+%AS% dosproc.asm
+%PC% -U..\blaster;..\main plays3m
 cd ..
