4417e63d9ac3679bae9af7c4de0750ebea6ac0d6
Ivan Tatarinov <ivan-tat@ya.ru>
2016-06-30 07:16:52 +0300

    add project cleaning script

---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+saferm() {
+	if [ -f "$1" ]; then rm "$1" > /dev/null; fi
+}
+
+echo "Cleaning project..."
+saferm ./blaster/*.tpu
+saferm ./main/*.obj
+saferm ./main/*.tpu
+saferm ./osci/*.obj
+saferm ./osci/*.exe
+saferm ./player/*.obj
+saferm ./player/*.exe
+echo "Done."
