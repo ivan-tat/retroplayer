fe94c6b7c43558833a955e4f7e2effbc6e280a92
Ivan Tatarinov <ivan-tat@ya.ru>
2016-07-05 22:11:01 +0300

    add pic.{c,h,pas} template files

---
+++ hw/pic.c
@@ -0,0 +1,12 @@
+/* pic.c -- Programmable Interuupt Controller interface.
+
+   This is free and unencumbered software released into the public domain */
+
+#ifdef __WATCOMC__
+#include <i86.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <conio.h>
+#endif
+
+#include "pic.h"
---
+++ hw/pic.h
@@ -0,0 +1,13 @@
+/* Declarations for pic.
+
+   This is free and unencumbered software released into the public domain */
+
+#ifndef _PIC_H
+#define _PIC_H 1
+
+#ifdef __WATCOMC__
+#include <stdbool.h>
+#include <stdint.h>
+#endif
+
+#endif /* _PIC_H */
