7952924652844747d92bce616956c8886bb94e70
Ivan Tatarinov <ivan-tat@ya.ru>
2016-07-08 21:16:03 +0300

    add dos.h for linking

---
+++ pascal/dos.h
@@ -0,0 +1,16 @@
+/* Declarations for DOS unit.
+
+   This is free and unencumbered software released into the public domain */
+
+#ifndef _PASCAL_DOS_H
+#define _PASCAL_DOS_H 1
+
+#ifdef __WATCOMC__
+#include <stdbool.h>
+#include <stdint.h>
+#endif
+
+extern void __far __pascal getintvec( uint8_t num, void **p );
+extern void __far __pascal setintvec( uint8_t num, void *p );
+
+#endif /* _PASCAL_DOS_H */
