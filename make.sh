8ebbdd6f8f8272eb94ce2ffe10ba71db3b503433
Ivan Tatarinov <ivan-tat@ya.ru>
2016-07-21 07:40:21 +0300

    make.sh: removed unnecessary parameter

--- make.sh
+++ make.sh
@@ -9,11 +9,13 @@ export WCC="-3 -fp3 -ml -na -oi -q -r -s -zdp -zff -zgf -zl -zls -zp=1 -zu"
 
 compile_c() {
 	local f_c="$1"
-	local segname="$2"
+	local segname="${f_c%.*}"
 	local f_obj="${f_c%.*}.obj"
 	local f_lst="${f_c%.*}.lst"
 	local f_tmp="${f_c%.*}.tmp"
 	local f_asm="${f_c%.*}.asm"
+    #segname="${segname^^}_TEXT" # such expansion is not supported by Bash-2.05b.0 in DJGPP
+    segname="`echo $segname | tr a-z A-Z`_TEXT"
 	wcc -nt=$segname "$f_c"
 	wdis -a "$f_obj" >"$f_tmp"
 	sed -r -e "s/(^DGROUP[[:space:]]+GROUP[[:space:]]+)CONST,CONST2,(_DATA)/\1\2/;\
@@ -41,26 +43,26 @@ done
 
 cd ../dos
 PASINC='-u..\pascal;..\watcomc'
-compile_c dosproc.c DOSPROC_TEXT
+compile_c dosproc.c
 $PC $PASINC dosproc.pas
-compile_c emstool.c EMSTOOL_TEXT
+compile_c emstool.c
 $PC $PASINC emstool.pas
 
 cd ../hw
 PASINC='-u..\pascal;..\watcomc'
-compile_c dma.c DMA_TEXT
+compile_c dma.c
 $PC $PASINC dma.pas
-compile_c pic.c PIC_TEXT
+compile_c pic.c
 $PC $PASINC pic.pas
 
 cd ../blaster
 PASINC='-u..\pascal;..\watcomc;..\hw'
 $ASW detisr_.asm
-compile_c detisr.c DETISR_TEXT
+compile_c detisr.c
 $PC $PASINC detisr.pas
-compile_c sbio.c SBIO_TEXT
+compile_c sbio.c
 $PC $PASINC sbio.pas
-compile_c sbctl.c SBCTL_TEXT
+compile_c sbctl.c
 $PC $PASINC sbctl.pas
 $PC $PASINC blaster.pas
 
@@ -72,9 +74,9 @@ $AS processo.asm
 $AS readnote.asm
 $PC $PASINC types.pas
 $PC $PASINC s3mtypes.pas
-compile_c voltab.c VOLTAB_TEXT
+compile_c voltab.c
 $PC $PASINC voltab.pas
-compile_c posttab.c POSTTAB_TEXT
+compile_c posttab.c
 $PC $PASINC posttab.pas
 $PC $PASINC s3mplay.pas
 
