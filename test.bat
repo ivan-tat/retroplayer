32f60894e8a4ef9ab93c23313a1377e265680a93
Tatarinov Ivan Ivanovich <ivan-tat@ya.ru>
2019-04-11 17:50:38 +0300

    add some player mode tests

--- test.bat
+++ test.bat
@@ -2,22 +2,37 @@
 
 set execpath=src\player
 set testsong=src\player\test.s3m
+set test_1=plays3m
+set test_2=plays3m -noems
+set test_3=plays3m -env -s22050 -m -8
+set test_4=plays3m -env -s22050 -8
+set test_5=plays3m -env -s22050 -m
+set test_6=plays3m -env
+set test_7=plays3m -env -noems
+set test_8=smalls3m
+set test_9=s3mosci
 
 :_start
 cls
 echo Choose a test to run (or press CTRL+C to exit):
 echo.
-echo 1. plays3m
-echo 2. plays3m -noems
-echo 3. plays3m -env
-echo 4. plays3m -env -noems
-echo 5. smalls3m
-echo 6. s3mosci
+echo 1. %test_1%
+echo 2. %test_2%
+echo 3. %test_3%
+echo 4. %test_4%
+echo 5. %test_5%
+echo 6. %test_6%
+echo 7. %test_7%
+echo 8. %test_8%
+echo 9. %test_9%
 echo.
 echo (using test song: %testsong%)
 echo.
-choice /C:123456
+choice /C:123456789
 
+if errorlevel 9 goto _9
+if errorlevel 8 goto _8
+if errorlevel 7 goto _7
 if errorlevel 6 goto _6
 if errorlevel 5 goto _5
 if errorlevel 4 goto _4
@@ -34,37 +49,55 @@ goto _start
 :_1
 echo.
 echo Running test 1...
-%execpath%\plays3m %testsong%
+%execpath%\%test_1% %testsong%
 goto _repeat
 
 :_2
 echo.
 echo Running test 2...
-%execpath%\plays3m -noems %testsong%
+%execpath%\%test_2% %testsong%
 goto _repeat
 
 :_3
 echo.
 echo Running test 3...
-%execpath%\plays3m -env %testsong%
+%execpath%\%test_3% %testsong%
 goto _repeat
 
 :_4
 echo.
 echo Running test 4...
-%execpath%\plays3m -env -noems %testsong%
+%execpath%\%test_4% %testsong%
 goto _repeat
 
 :_5
 echo.
 echo Running test 5...
-%execpath%\smalls3m %testsong%
+%execpath%\%test_5% %testsong%
 goto _repeat
 
 :_6
 echo.
 echo Running test 6...
-%execpath%\s3mosci %testsong%
+%execpath%\%test_6% %testsong%
+goto _repeat
+
+:_7
+echo.
+echo Running test 7...
+%execpath%\%test_7% %testsong%
+goto _repeat
+
+:_8
+echo.
+echo Running test 8...
+%execpath%\%test_8% %testsong%
+goto _repeat
+
+:_9
+echo.
+echo Running test 9...
+%execpath%\%test_9% %testsong%
 goto _repeat
 
 :_end
