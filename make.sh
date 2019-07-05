a3a79a90065764ceeb55e7c405bbf4e2c51b7d3d
Tatarinov Ivan Ivanovich <ivan-tat@ya.ru>
2019-07-05 23:06:47 +0300

    clean make.sh script, add options description in src/Makefile, remove unneeded tpc.cfg files

--- make.sh
+++ make.sh
@@ -10,51 +10,8 @@ if [ -z "$target" ]; then
     target=all
 fi
 
-T_CC=0
-T_DEBUG=0
-T_DOS=0
-T_MISC=0
-T_HW=0
-T_HW_SB=0
-T_MAIN=0
-T_PLAYER=0
-T_WATCOM=1
-T_TP=1
-
 case "$target" in
     all)
-        T_CC=1
-        T_DEBUG=1
-        T_DOS=1
-        T_MISC=1
-        T_HW=1
-        T_HW_SB=1
-        T_MAIN=1
-        T_PLAYER=1
-        ;;
-    cc)
-        T_CC=1
-        ;;
-    debug)
-        T_DEBUG=1
-        ;;
-    dos)
-        T_DOS=1
-        ;;
-    misc)
-        T_MISC=1
-        ;;
-    hw)
-        T_HW=1
-        ;;
-    hw_sb)
-        T_HW_SB=1
-        ;;
-    main)
-        T_MAIN=1
-        ;;
-    player)
-        T_PLAYER=1
         ;;
     *)
         echo "Error: unknown target \"$target\"."
@@ -62,299 +19,28 @@ case "$target" in
         ;;
 esac
 
-if [ $T_TP == 1 ]; then
-    if [ $T_CC == 1 ]; then
-        T_CC_TP=1
-    fi
-    if [ $T_DEBUG == 1 ]; then
-        T_DEBUG_TP=1
-    fi
-    if [ $T_DOS == 1 ]; then
-        T_DOS_TP=1
-    fi
-    if [ $T_MISC == 1 ]; then
-        T_MISC_TP=1
-    fi
-    if [ $T_HW == 1 ]; then
-        T_HW_TP=1
-    fi
-    if [ $T_HW_SB == 1 ]; then
-        T_HW_SB_TP=1
-    fi
-    if [ $T_MAIN == 1 ]; then
-        T_MAIN_TP=1
-    fi
-    if [ $T_PLAYER == 1 ]; then
-        T_PLAYER_TP=1
-    fi
-    if [ $T_WATCOM == 1 ]; then
-        T_WATCOM_TP=1
-    fi
-else
-    T_CC_TP=0
-    T_DEBUG=0
-    T_DOS_TP=0
-    T_MISC_TP=0
-    T_HW_TP=0
-    T_HW_SB_TP=0
-    T_MAIN_TP=0
-    T_PLAYER_TP=0
-    T_WATCOM_TP=0
-fi
-
 echo "Building for target \`$target' ..."
 
 PROJDIR="$PWD"
 LASTDIR="$PROJDIR"
 
-W_AS="wasm -zq"
-# Description of options:
-# -zq             operate quietly
-W_CC='wcc'
-export WCC="-3 -fp3 -ml -oi -oc -os -q -r -s -zdp -zff -zgf -zl -zld -zls -zp=1 -zu"
-# Description of options for "wcc":
-# -3              386 instructions
-# -fp3            generate floating-point code (80387 FPU code)
-# -ml             memory model (large - large code/large data)
-# -ecc            set default calling convention to __cdecl
-# -ecp            set default calling convention to __pascal
-# -ecw            set default calling convention to __watcall (default)
-# -oc             disable <call followed by return> to <jump> optimization
-#                 reason: "wdis" incorrectly writes "je near ptr <near_extern_label>"
-#                 (without "near ptr")
-# -oi             expand intrinsic functions inline
-# -os             favor code size over execution time in optimizations
-# -q              operate quietly (display only error messages)
-# -r              save/restore segment registers across calls
-# -s              remove stack overflow checks
-# -zdp            DS is pegged to DGROUP
-# -zff            FS floats (i.e. not fixed to a segment)
-# -zgf            GS floats (i.e. not fixed to a segment)
-# -zl             remove default library information
-# -zld            remove file dependency information
-# -zls            remove automatically inserted symbols
-# -zp=1           pack structure members with alignment (1)
-# -zu             SS != DGROUP (i.e., do not assume stack is in data segment)
-W_CL='wcl'
-W_CL386='wcl386'
-W_DIS='wdis'
 W_LIB='wlib'
-PC='tpc -gd -q -v -$d+,e-,g+,l+,n+'
+
+PC='tpc'
+PCFLAGS='-gd -m -v -$d+,e-,g+,l+,n+'
 # Description of options for "tpc":
-# -gd             detailed map file
-# -q              quiet compile
-# -v              debug information in EXE
-# -$d+            debug information (yes)
-# -$e-            80x87 emulation (no)
-# -$g+            80286 instructions (yes)
-# -$l+            local debug symbols (yes)
-# -$n+            80x87 instructions (yes)
-
-_dir="$PROJDIR/src"
-if [ -n "$INCLUDE" ]; then
-    export INCLUDE="$INCLUDE;$_dir"
-else
-    export INCLUDE="$_dir"
-fi
-unset _dir
-
-ch_dir() {
-    local f_locname="$1"
-    local f_cd="$2"
-    local f_dir="${f_locname%/*}"
-    local newdir
-    if [ $f_cd == 1 ]; then
-        echo "--- File: $f_locname ---"
-    else
-        echo "--- Skip: $f_locname ---"
-    fi
-    if [ "$f_dir" == "$f_locname" ]; then
-        newdir="$PROJDIR"
-    else
-        newdir="$PROJDIR/$f_dir"
-    fi
-    if [ "$LASTDIR" != "$newdir" ]; then
-        cd "$newdir"
-    fi
-}
-
-recompile_obj() {
-    local f_obj="$1"
-    local f_nam="${f_obj%.obj}"
-    local f_tmp="${f_nam}.tmp"
-    local f_asm="${f_nam}.asm"
-    $W_DIS -a -fi "$f_obj" >"$f_tmp"
-    sed -r -e "s/(^DGROUP[[:space:]]+GROUP[[:space:]]+)CONST,CONST2,(_DATA)/\1\2/;\
-s/^CONST[2]?([[:space:]]+(SEGMENT[[:space:]]+.+*|ENDS[[:space:]]*)$)/_DATA\1/;\
-s/([[:space:]]|,|-|\+)(0[a-f]{1}[[:xdigit:]]{0,7}|[1-9]{1}[[:xdigit:]]{0,7})([[:xdigit:]]{8}H)/\10\3/g;\
-s/(.+\,DGROUP:)CONST$/\1_DATA/;" \
-"$f_tmp" >"$f_asm"
-    rm -f "$f_tmp"
-    $W_AS -fo="$f_obj" "$f_asm"
-}
-
-mkobj_c() {
-    local f_c="$1"
-    local f_nam="${f_c%.c}"
-    local f_obj="${f_nam}.obj"
-    local segname
-    # such expansion works in Bash-4.1.17 release 1 (not in Bash-2.05b release 1) in DJGPP V2:
-    segname="${f_nam^^}_TEXT"
-    # workaraound for Bash-2.05b release 1 in DJGPP V2:
-    #segname="`echo $f_nam | tr a-z A-Z`_TEXT"
-    $W_CC -nt=$segname -fo="$f_obj" "$f_c"
-    recompile_obj "$f_obj"
-}
-
-mkobj() {
-    local f_locname="$1"
-    local f_dir="${f_locname%/*}"
-    local f_name="${f_locname##*/}"
-
-    case "${f_name##*.}" in
-        asm)
-            if [ ! -f "$PROJDIR/$f_dir/${f_name%.*}.obj" ]; then
-                ch_dir "$f_locname" 1
-                $W_AS -fo="${f_name%.asm}.obj" "$f_name"
-            else
-                ch_dir "$f_locname" 0
-            fi
-            ;;
-        c)
-            if [ ! -f "$PROJDIR/$f_dir/${f_name%.*}.obj" ]; then
-                ch_dir "$f_locname" 1
-                mkobj_c "$f_name"
-            else
-                ch_dir "$f_locname" 0
-            fi
-            ;;
-        pas)
-            if [ ! -f "$PROJDIR/$f_dir/${f_name%.*}.tpu" ]; then
-                ch_dir "$f_locname" 1
-                $PC "$f_name"
-            else
-                ch_dir "$f_locname" 0
-            fi
-            ;;
-        *)
-            echo "Error: Do not known how to build source \`$f_locname'."
-            return 1;
-            ;;
-    esac
-}
-
-mkexe_c_dos() {
-    local f_name="$1"
-    local f_obj="${f_name%.*}.obj"
-    local f_exe="${f_name%.*}.exe"
-    local WCL_OLD="$WCL"
-    export WCL="-q -3 -fp3 -fpi87 -om -bcl=dos"
-    $W_CL -fo="$f_obj" -fe="$f_exe" "$f_name"
-    rm -f "$f_obj"
-    export WCL="$WCL_OLD"
-}
-
-mkexe_c_linux() {
-    local f_name="$1"
-    local f_obj="${f_name%.*}.o"
-    local f_exe="${f_name%.*}"
-    local WCL386_OLD="$WCL386"
-    export WCL386="-q -3 -fp3 -fpi87 -om -bcl=linux"
-    $W_CL386 -fo="$f_obj" -fe="$f_exe" "$f_name"
-    rm -f "$f_obj"
-    export WCL386="$WCL386_OLD"
-}
-
-mkinc_linux() {
-    local f_locname="$1"
-    local f_dir="${f_locname%/*}"
-    local f_name="${f_locname##*/}"
-    local f_exe="${f_name%.*}"
-    local f_inc="${f_name%.*}.inc"
-    local domake
-
-    if [ -f "$PROJDIR/$f_dir/$f_inc" ]; then
-        domake=0
-    else
-        domake=1
-    fi
-
-    ch_dir "$f_locname" "$domake"
-
-    if [ $domake == 1 ]; then
-        case "${f_name##*.}" in
-            c)
-                if [ -f "$PROJDIR/$f_dir/$f_exe" ]; then
-                    rm -f "$PROJDIR/$f_dir/$f_exe"
-                fi
-                mkexe_c_linux "$f_name"
-                "./$f_exe" > "$f_inc"
-                rm -f "$f_exe"
-                ;;
-            *)
-                echo "Error: Do not known how to build source \`$f_locname' in GNU/Linux."
-                return 1;
-                ;;
-        esac
-    fi
-}
-
-mkexe_dos() {
-    local f_locname="$1"
-    local f_dir="${f_locname%/*}"
-    local f_name="${f_locname##*/}"
-    local f_exe="${f_name%.*}.exe"
-    local domake
-
-    if [ -f "$PROJDIR/$f_dir/$f_exe" ]; then
-        domake=0
-    else
-        domake=1
-    fi
-
-    ch_dir "$f_locname" "$domake"
-
-    if [ $domake == 1 ]; then
-        case "${f_name##*.}" in
-            c)
-                mkexe_c_dos "$f_name"
-                ;;
-            pas)
-                $PC "$f_name"
-                ;;
-            *)
-                echo "Error: Do not known how to build source \`$Source' in DOS."
-                return 1;
-                ;;
-        esac
-    fi
-}
-
-build_target() {
-    local Target=$1
-    local BuildType=$2
-    local Source="$3"
-    if [ $Target -eq 1 ]; then
-        case $BuildType in
-            inc-linux)
-                mkinc_linux "$Source"
-                ;;
-            exe-dos)
-                mkexe_dos "$Source"
-                ;;
-            obj)
-                mkobj "$Source"
-                ;;
-            *)
-                echo "Error: Unknown build type \`$BuildType' for source \`$Source'."
-                return 1
-                ;;
-        esac
-    fi
-}
+# -gd   detailed map file
+# -m    make modified units
+# -q    quiet compile
+# -v    debug information in EXE
+# -$d+  debug information (yes)
+# -$e-  80x87 emulation (no)
+# -$g+  80286 instructions (yes)
+# -$l+  local debug symbols (yes)
+# -$n+  80x87 instructions (yes)
 
 if [ -z "$DJGPP" ]; then
-if [ $T_WATCOM == 1 ]; then
+# !DJGPP
     echo 'Extracting Watcom C specific files...'
     . ./scripts/ow/ow.sh
     . ./scripts/ow/clib.sh
@@ -364,79 +50,19 @@ if [ $T_WATCOM == 1 ]; then
     mv i4m.o i4m.obj
     mv i8d086.o i8d086.obj
     cd "$PROJDIR"
-fi
 
-if [ -n "$T_CC" ]; then
     if [ ! -f "$PROJDIR/src/cc/cc.lib" ]; then
         tmp=`mktemp`
         find "$PROJDIR/src/cc/" -type f -name '*.obj' -printf '+%p ' | sort -u > $tmp
         wlib -q -t -zld -o "$PROJDIR/src/cc/cc.lib" @$tmp
         rm $tmp
     fi
-fi
-fi
-
-if [ -n "$DJGPP" ]; then
-_dir="$INCLUDE"
-if [ -n "$_dir" ]; then
-    # replace slashes "/" with backslashes "\" for DOS compatibility
-    _dir="${_dir//\//\\}"
-    export INCLUDE="$_dir"
-fi
-unset _dir
-build_target $T_WATCOM_TP obj src/watcom.pas
-build_target $T_TP        obj src/pascal.pas
-build_target $T_TP        obj src/strutils.pas
-build_target $T_CC_TP     obj src/cc/i86.pas
-build_target $T_CC_TP     obj src/cc/errno_.pas
-build_target $T_CC_TP     obj src/cc/dos_.pas
-build_target $T_CC_TP     obj src/cc/string_.pas
-build_target $T_HW_TP     obj src/hw/cpu.pas
-build_target $T_MISC_TP   obj src/startup.pas
-build_target $T_MISC_TP   obj src/common.pas
-build_target $T_HW_TP     obj src/hw/vbios.pas
-build_target $T_CC_TP     obj src/cc/conio.pas
-build_target $T_CC_TP     obj src/cc/ctype.pas
-build_target $T_CC_TP     obj src/cc/malloc.pas
-build_target $T_CC_TP     obj src/cc/io.pas
-build_target $T_CC_TP     obj src/cc/stdlib.pas
-build_target $T_CC_TP     obj src/cc/dstream.pas
-build_target $T_CC_TP     obj src/cc/printf.pas
-build_target $T_CC_TP     obj src/cc/stdio.pas
-build_target $T_CC_TP     obj src/cc/unistd.pas
-build_target $T_DEBUG_TP  obj src/debug.pas
-build_target $T_DOS_TP    obj src/dos/ems.pas
-build_target $T_MISC_TP   obj src/dynarray.pas
-build_target $T_HW_TP     obj src/hw/hwowner.pas
-build_target $T_HW_TP     obj src/hw/dma.pas
-build_target $T_HW_TP     obj src/hw/pic.pas
-build_target $T_HW_TP     obj src/hw/sndctl_t.pas
-build_target $T_HW_TP     obj src/hw/vga.pas
-build_target $T_HW_SB_TP  obj src/hw/sb/sbio.pas
-build_target $T_HW_SB_TP  obj src/hw/sb/sbctl.pas
-build_target $T_MAIN_TP   obj src/main/types.pas
-build_target $T_MAIN_TP   obj src/main/s3mtypes.pas
-build_target $T_MAIN_TP   obj src/main/pcmsmp.pas
-build_target $T_MAIN_TP   obj src/main/musins.pas
-build_target $T_MAIN_TP   obj src/main/muspat.pas
-build_target $T_MAIN_TP   obj src/main/musmod.pas
-build_target $T_MAIN_TP   obj src/main/effvars.pas
-build_target $T_MAIN_TP   obj src/main/fillvars.pas
-build_target $T_MAIN_TP   obj src/main/voltab.pas
-build_target $T_MAIN_TP   obj src/main/posttab.pas
-build_target $T_MAIN_TP   obj src/main/mixer.pas
-build_target $T_MAIN_TP   obj src/main/mixchn.pas
-build_target $T_MAIN_TP   obj src/main/musmodps.pas
-build_target $T_MAIN_TP   obj src/main/effects.pas
-build_target $T_MAIN_TP   obj src/main/readnote.pas
-build_target $T_MAIN_TP   obj src/main/mixing.pas
-build_target $T_MAIN_TP   obj src/main/filldma.pas
-build_target $T_MAIN_TP   obj src/main/loads3m.pas
-build_target $T_MAIN_TP   obj src/main/s3mplay.pas
-build_target $T_PLAYER_TP obj src/player/screen.pas
-build_target $T_PLAYER_TP exe-dos src/player/plays3m.pas
-build_target $T_PLAYER_TP exe-dos src/player/smalls3m.pas
-build_target $T_PLAYER_TP exe-dos src/player/playosci.pas
+else
+# DJGPP
+    cd "$PROJDIR/src"
+    $PC $PCFLAGS 'player\plays3m.pas'
+    $PC $PCFLAGS 'player\smalls3m.pas'
+    $PC $PCFLAGS 'player\playosci.pas'
 fi
 
 cd "$PROJDIR"
